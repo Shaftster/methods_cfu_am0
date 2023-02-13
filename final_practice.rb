15e27ef28c67519dfba6b02a611891f4e3e83ae7
Jermaine Wates <jwates7@protonmail.com>
2023-02-13 02:35:50 -0700

    finished final practice

--- final_practice.rb
+++ final_practice.rb
@@ -8,6 +8,9 @@
 def greet_a_new_customer
     return "Thanks for being a new customer"
 end
+
+p greet_a_new_customer
+
 # What is the return value of your method?
 
     # 'Thanks for being a new customer
@@ -15,31 +18,67 @@ end
 
 
 # 2: Write a method named custom_greeting that returns a greeting WITH a specific name.
-new_friend = ("name")
-def greet_a_new_friend (name)
-    return 'pleasure to meet you #{name}'
+
+
+def greet_a_new_friend(name)
+    return "pleasure to meet you #{name}"
 end
 
-p greet_a_new_friend "john"
+p greet_a_new_friend("john")
 
 # What is the return value of your method?
+    # "john"
 # How many arguments did you pass your method?
+    # "one"
 # What data type was your argument(s)?
+    # "string"
 
 
 # 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
+def greet_person(name1, name2, name3)
+    return  "Welcome to Turing, #{name1} #{name2} #{name3}!"
 
+end
+
+p greet_person("Lucielle", "Brenda", "Thompkins")
+    
 # What is the return value of your method?
+
+# 'welcome to turing Lucielle Brenda Thompkins
+
 # How many arguments did you pass your method?
+
+# three
+
 # What data type was your argument(s)?
+
+# strings
 
 
 # 4: Write a method named square that takes in one integer, and returns the square of that integer.
 # Bonus: Print a sentence that interpolates the return value of your square method.
 
+def square(number)
+    return number * number
+    # puts "the square of this number is #{square.}" #I Tried
+end
+p square 5
+
+
+
+
 # What is the return value of your method?
+
+#25
+
 # How many arguments did you pass your method?
+
+# one
+
+
 # What data type was your argument(s)?
+
+#integer
 
 
 # 5: Write a method named check_stock that satisfies the following interaction pattern:
@@ -48,11 +87,45 @@ p greet_a_new_friend "john"
 # check_stock(4, "Coffee");
 # => "Coffee is stocked"
 
+def check_stock(number)
+
+    if number > (4)
+        return "coffee is stocked"
+    end
+end
+
+p check_stock (5)
+
+
 # check_stock(3, "Tortillas");
 # => "Tortillas - running LOW"
+
+def check_stock(number)
+
+    if number <= 3
+        return "running LOW"
+    end
+end
+p check_stock (3)
 
 # check_stock(0, "Cheese");
 # => "Cheese - OUT of stock!"
 
+def check_stock(number)
+
+    if number <= 0
+        return "OUT of stock!"
+    end
+end
+p check_stock (0)
+
 # check_stock(1, "Salsa");
 # => "Salsa - running LOW"
+
+def check_stock(number)
+    if number <= 1
+        return "running LOW"
+    end 
+end
+
+p check_stock (1)
